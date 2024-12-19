4bb92a4d76a34a1a7755ae265fb606eef2faed9f
DaniyarRa <d_raiymbekov@kbtu.kz>
2024-12-19 13:16:00 +0500

    init commit 3

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,14 +2,15 @@ from fastapi import APIRouter, Request
 from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+from transformers import AutoTokenizer
+import onnxruntime
+import numpy as np
+import torch
+import torch.nn.functional as F
+import json
 
-tokenizer = AutoTokenizer.from_pretrained('yeshpanovrustem/xlm-roberta-large-ner-kazakh')
-model = AutoModelForTokenClassification.from_pretrained('yeshpanovrustem/xlm-roberta-large-ner-kazakh')
-
-nlp_none = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="none")
-nlp_simple = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="simple")
-
+tokenizer = AutoTokenizer.from_pretrained('tokenizer_onnx')
+ort_session = onnxruntime.InferenceSession('tokenizer_onnx/model.onnx', providers=["CPUExecutionProvider"])
 
 router = APIRouter()
 
@@ -26,40 +27,66 @@ async def get_page():
 @router.post("/label")
 async def submit_text(request: Request):
     data = await request.json()
-    text = data["text"]
-    option = data["option"]
+    text = data['text']
+    probabilities, input_ids = run_ner_inference(text)
 
-    ner_results, message = None, 'N/A'
-    if option == "Simple":
-        message = _normalize_simple_ner(nlp_simple(text))
-    elif option == "None":
-        message = _normalize_ner(nlp_none(text))
+    predicted_classes = np.argmax(probabilities, axis=-1)
+
+    config = json.load(open('tokenizer_onnx/config.json'))
+    id2label = config['id2label']
+
+    predicted_labels = [id2label[str(class_id)] for class_id in predicted_classes[0]]
+
+    tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+    clean_tokens, clean_labels = clean_and_combine_tokens(tokens, predicted_labels)
+
+    message = ' '.join([label if label in ['B-CONTACT', 'B-PERSON'] else token for token, label in zip(clean_tokens, clean_labels)])
 
     return {"message": message}
 
 
-def _normalize_simple_ner(ner_results):
-    result = '\n'.join([f'{result["word"]}\t{result["entity_group"]}' for result in ner_results])
+def run_ner_inference(input_text):
+    encoded_input = tokenizer(input_text, padding=True, truncation=True, return_tensors="pt", add_special_tokens=True)
+    input_ids = encoded_input['input_ids'].numpy()
+    attention_mask = encoded_input['attention_mask'].numpy()
 
-    return result
+    ort_inputs = {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask
+    }
+
+    ort_outs = ort_session.run(['logits'], ort_inputs)
+    logits = ort_outs[0]
+
+    probabilities = F.softmax(torch.from_numpy(logits), dim=-1).numpy()
+
+    return probabilities, input_ids
 
 
-def _normalize_ner(ner_results):
-    token = ""
-    label_list = []
-    token_list = []
+def clean_and_combine_tokens(tokens, labels):
+    cleaned_tokens = []
+    cleaned_labels = []
+    current_word = ""
+    current_label = None
 
-    for result in ner_results:
-        if result["word"].startswith("▁"):
-            if token:
-                token_list.append(token.replace("▁", ""))
-            token = result["word"]
-            label_list.append(result["entity"])
+    for token, label in zip(tokens, labels):
+        if token in ["<s>", "</s>", "<unk>"]:
+            continue
+
+        if token.startswith("▁"):
+            if current_word:
+                cleaned_tokens.append(current_word)
+                cleaned_labels.append(current_label)
+            current_word = token[1:]
+            current_label = label
         else:
-            token += result["word"]
+            current_word += token
 
-    token_list.append(token.replace("▁", ""))
+        if current_label == "O":
+            current_label = label
 
-    result = '\n'.join([f'{token}\t{label}' for token, label in zip(token_list, label_list)])
+    if current_word:
+        cleaned_tokens.append(current_word)
+        cleaned_labels.append(current_label)
 
-    return result
+    return cleaned_tokens, cleaned_labels
